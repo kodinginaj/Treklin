ee979e7dcb18b463387237c197ec8d370aba648d
M Reza Khalafi <mrezakhalafi@yahoo.com>
2020-06-05 23:32:49 +0700

    Proses

---
+++ app/src/main/java/com/example/treklin/model/ResponseModel.java
@@ -0,0 +1,54 @@
+package com.example.treklin.model;
+
+public class ResponseModel {
+
+    private String status, message, role, id, nama;
+
+    public ResponseModel(String status, String message, String role, String id, String nama) {
+        this.status = status;
+        this.message = message;
+        this.role = role;
+        this.id = id;
+        this.nama = nama;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
